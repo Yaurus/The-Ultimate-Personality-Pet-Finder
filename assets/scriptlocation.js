30fe6c1263558e66726a34728713301b55411055
jojo <jodiladouceur.design@gmail.com>
2021-03-27 13:17:11 -0400

    edits to files

---
+++ assets/scriptlocation.js
@@ -0,0 +1,3 @@
+// Key for the google maps api AIzaSyCVUfhv4uKOfDaY8KDu1IrslT3EPgi49qU
+// link for api https://maps.googleapis.com/maps/api/place/nearbysearch/output?parameters
+// example link https://maps.googleapis.com/maps/api/place/textsearch/xml?query=restaurants+in+Sydney&key=YOUR_API_KEY
