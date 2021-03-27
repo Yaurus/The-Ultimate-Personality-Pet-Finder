a5f34a5313ef642f9cc4fd2075654b389ab30914
stephan du Plooy <sdpphotography1@gmail.com>
2021-03-27 13:46:45 -0400

    Added Questions and made program change pages to quiz page

---
+++ script.js
@@ -0,0 +1,6 @@
+var startBtn = $("startBtn")
+
+//function that switches pages
+function start() {
+    window.location.href = "file:///C:/Users/Stephan/Pictures/Course/The-Ultimate-Personality-Pet-Finder/quiz.html";
+}
---
+++ scriptquiz.js
@@ -0,0 +1,14 @@
+var questions = [
+    {
+        title: "Do you have a large Apartment?",
+        choices: ["Yes", "No"],
+        answers: ["Yes", "No"]
+    },
+
+    {
+        title: "Do you have a backyard?",
+        choices: ["Yes", "No"],
+        answers: ["Yes", "No"]
+    }
+]
+
