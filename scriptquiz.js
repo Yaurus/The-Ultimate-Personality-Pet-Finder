4227be333bce1d23741f7f3ac0c5bb6f6784f9e3
stephan du Plooy <sdpphotography1@gmail.com>
2021-03-28 16:36:17 -0400

    Added Beginning of Quiz Questions

--- scriptquiz.js
+++ scriptquiz.js
@@ -1,3 +1,5 @@
+
+//array for all of the questions, choices, and answers
 var questions = [
     {
         title: "Do you have a large Apartment?",
@@ -11,4 +13,61 @@ var questions = [
         answers: ["Yes", "No"]
     }
 ]
+
+var currentQuestion = -1;
+var question1Btnyes = $(".yes")
+var question1 = $(".question1");
+var question2 = $(".question2");
+
+//stores yes answers into console
+function Yesanswer1 () {
+    console.log("Yes")
+    var yes = document.querySelector(".yes");
+    console.log(yes);
+    localStorage.setItem(".yes", yes); 
+    $(".question1").hide();
+    $(".question2").addClass("shown");
+}
+
+//stores yes answers into console
+function Noanswer1 () {
+    console.log("No")
+    var no = document.querySelector(".no");
+    console.log(no);
+    localStorage.setItem(".no", no); 
+    $(".question1").hide();
+    $(".question2").addClass("shown");
+}
+
+//stores yes answers into console for question2
+function Yesanswer2() {
+    console.log("Yes")
+    var yes2 = document.querySelector(".yes2");
+    console.log(yes2);
+    localStorage.setItem(".yes2", yes2); 
+    $(".question2").hide();
+    // $(".question2").addClass("shown");
+}
+
+//stores yes answers into console for question2
+function Yesanswer2() {
+    console.log("Yes")
+    var yes2 = document.querySelector(".yes2");
+    console.log(yes2);
+    localStorage.setItem(".yes2", yes2); 
+    $(".question2").hide();
+    // $(".question2").addClass("shown");
+}
+
+//stores no answers into console for question2
+function Noanswer2() {
+    console.log("No")
+    var no2 = document.querySelector(".no2");
+    console.log(no2);
+    localStorage.setItem(".no2", no2); 
+    $(".question2").hide();
+    // $(".question2").addClass("shown");
+}
+
+
 
