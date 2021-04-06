8768db00dbc88fbfe6c1e4d559899949eb30c672
jojo <jodiladouceur.design@gmail.com>
2021-04-06 19:11:03 -0400

    edits to files and layout of files

--- script.js
+++
@@ -1,7 +0,0 @@
-var startBtn = $("startBtn")
-
-//function that switches pages
-function start() {
-    window.location.href = "file:///C:/Users/Stephan/Pictures/Course/The-Ultimate-Personality-Pet-Finder/quiz.html";
-}
-
