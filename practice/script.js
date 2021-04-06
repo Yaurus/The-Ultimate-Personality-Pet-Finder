cae902187af5c8cebfc171bd4d36a78a9e1c1ced
jojo <jodiladouceur.design@gmail.com>
2021-04-06 09:58:58 -0400

    edits to files and added new files

--- practice/script.js
+++ practice/script.js
@@ -1,6 +1,8 @@
 var apiKey = 'LWxSqYatdplE3OeXhPTKjm1PHsfDDVflXSPguuUtViYdrQMY1c'; // assign our key to a variable, easier to read
 var secret = "kWGAVp8RvLCQqmYSGVA9danmtZydlMw6CufLt6CR"
 var grant_type = "client_credentials"
+var access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJMV3hTcVlhdGRwbEUzT2VYaFBUS2ptMVBIc2ZERFZmbFhTUGd1dVV0VmlZZHJRTVkxYyIsImp0aSI6ImY2ODE4ZGNkZjEwNzgzNjc1NzA5YzM4MDExMGVmNTkxMTNmYjU2NjcxNTI3ZDkxZWU2ZTRhOGZiZGFjMDAxOTQwNjU4ZWZlY2EzMmMxODg5IiwiaWF0IjoxNjE3NDU5NTUwLCJuYmYiOjE2MTc0NTk1NTAsImV4cCI6MTYxNzQ2MzE1MCwic3ViIjoiIiwic2NvcGVzIjpbXX0.YD3XMa97neb5YMz0VGEiAPxcPZL_cl7VPDu5UjpGXe6yxWxPv3EDFVxV_hfE0iS_MmtbHTwnAGqy8-tRuE0mtRdTIdZV5XIhATQvijBlE00FzQnSFoXPVFOOKoY90t3eNnJu1JiYxsShKFX1BiqAsLmRfVrTo4a746HLrgZAlOV7XzIUIMvNUV-r8urBfeiYN1PTmNq4qb3yTJngl60fZw8iqdDxJ-SE8W8jC9mrd985u5n-Ee-s8IPuQplId57ksJnIoIwMeFPU88NQV_xvzb-lJQuSYmoGlVWAQQcgNI-v9fz8vJUsNomyMr2UPnvmHyczF6jUECmFitDj6gi5aA"
+var token, tokenType, expire;
 
 // the next line and function set up the button in our html to be clickable and reactive 
 document.addEventListener('DOMContentLoaded', bindButtons);
@@ -10,7 +12,8 @@ post = `https://cors-anywhere.herokuapp.com/https://api.petfinder.com/v2/oauth2/
 details = {
     'grant_type': grant_type,
     'client_id': apiKey,
-    'client_secret': secret
+    'client_secret': secret,
+	'token': access_token,
 }
 
 var formBody = [];
